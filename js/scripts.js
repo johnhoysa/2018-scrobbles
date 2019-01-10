c6954c837cb0db8c7e93953a548c233ff2a680fe
John Hoysa <johnhoysa@gmail.com>
2019-01-09 18:24:15 -0600

    updated some comments

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,23 @@ var header = document.querySelector("header");
 var section = document.querySelector("section");
 
 //Create variable for json data
-const requestUrl =
+const requestTopAlbumsURL =
   "http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=jhoysa&api_key=2415395c4acbe6c072c34ed1ccb9f676&format=json&limit=50";
 
-var request = new XMLHttpRequest();
-request.open("GET", requestUrl);
-request.responseType = "text";
-request.send();
+var requestTopAlbums = new XMLHttpRequest();
+requestTopAlbums.open("GET", requestTopAlbumsURL);
+requestTopAlbums.responseType = "text";
+requestTopAlbums.send();
 
-request.onload = function() {
-  var myAlbumsText = request.response;
+requestTopAlbums.onload = function() {
+  var myAlbumsText = requestTopAlbums.response;
   var myAlbums = JSON.parse(myAlbumsText);
 
-  //console.log(myAlbums.topalbums.album);
   showAlbums(myAlbums);
 };
 
 function showAlbums(jsonObj) {
   let albumInfo = jsonObj["topalbums"];
-  //console.log(albumInfo);
-  //console.log(albumInfo.album[3].image[]);
 
   for (var i = 0; i < albumInfo.album.length; i++) {
     if (albumInfo.album[i].image[3]["#text"] == "") {
@@ -32,9 +29,7 @@ function showAlbums(jsonObj) {
 
     displayAlbums.innerHTML += `
       <div class='album'>
-        
         <img src="${imageSource}" alt="cover art for" width="" height ="">
-
         <p>Album Name: ${albumInfo.album[i].name}</p>
         <p>artist Name: ${albumInfo.album[i].artist.name}</p>
       </div>
@@ -42,8 +37,4 @@ function showAlbums(jsonObj) {
 
     console.log(albumInfo.album[i].image[2]["#text"]);
   }
-
-  // var myPara1 = document.createElement("p");
-  // myPara1.textContent = "Album Name: " + albumInfo.album[i].name;
-  // header.appendChild(myPara1);
 }
